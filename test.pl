6675532203ed1654e19892aaaaf33327853b6ded
Terrence Brannon <metaperl@gmail.com>
2000-02-17 17:15:14 +0000

    Import of TBONE/Date-Ordinal-2.4 from CPAN.
    gitpan-cpan-distribution: Date-Ordinal
    gitpan-cpan-version:      2.4
    gitpan-cpan-path:         TBONE/Date-Ordinal-2.4.tar.gz
    gitpan-cpan-author:       TBONE
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -1,28 +1,9 @@
 #!/usr/bin/perl
 
 use lib '/usr/end70/mnt/admin/perl';
-use Locale::US;
+use Date::Ordinal;
 
-
-$code='CA';
-$state='California';
-
-print code2state($code), $/;
-print state2code($state), $/;
-print join ':', sort ( &all_state_codes() ), $/;
-print join ':', sort ( &all_state_names() ), $/;
-
-%C = &all_code_hash;
-%S = &all_state_hash;
-
-
-print $C{'Wyoming'},$/;
-print $S{WY},$/;
-
-
-$R=&all_state_codes_ref();
-print @{$R},$/;
-
-$R=&all_state_names_ref();
-print @{$R},$/;
+print ord2month(5),$/;
+print month2ord('May'),$/;
+print join ':', @{&Date::Ordinal::all_month_ordinations}, $\;
 
